ac3920aef97b126037c1315366404f087bb911c1
tictools <jaumevillarreal@gmail.com>
2024-05-10 18:15:16 +0200

    feat: add deploy info

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # notekeeper
 
 NoteKepper is a SPA built with Vite and implemented using Domain-Driven Design (DDD). This project offers a seamless platform for organizing, managing, and accessing your notes.
+
+### Deploy
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/be3f990b-ecff-4dbb-bc8f-1f261f8bf57d/deploy-status)](https://app.netlify.com/sites/notekeeper-ddd-app/deploys)
+
+https://notekeeper-ddd-app.netlify.app/
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const RESORUCE_PATH = "notes";
+
 function App() {
   useEffect(() => {
     globalThis
-      // .fetch(`${import.meta.env.VITE_API_BASE_URL}/notes`)
-      .fetch(`https://notekeeper-api-x9ji.onrender.com/notes`)
+      .fetch(`${API_BASE_URL}/${RESORUCE_PATH}`)
       .then((response) => response.json())
       .then((notes) => console.log(notes))
       .catch((error) => console.log(error));
