b3ad7bbaf6fb6bfdede633bea1910a09f543599b
tictools <jaumevillarreal@gmail.com>
2024-05-11 09:36:43 +0200

    feat: add basic component render for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import styles from "./App.module.css";
+
+const { VITE_API_BASE_URL, PACKAGE_VERSION } = import.meta.env;
+const RESORUCE_PATH = "notes";
 
 function App() {
-  const { API_BASE_URL, PACKAGE_VERSION } = import.meta.env;
-  const RESORUCE_PATH = "notes";
+  enum NoteStatus {
+    pending = "pending",
+    inProgress = "in progress",
+    done = "done",
+  }
 
-  console.log(import.meta.env.PACKAGE_VERSION);
+  type Note = {
+    name: string;
+    description: string;
+    status: NoteStatus;
+    important: boolean;
+  };
+
+  const [notes, setNotes] = useState<Note[] | []>([]);
+
   useEffect(() => {
     globalThis
-      .fetch(`${API_BASE_URL}/${RESORUCE_PATH}`)
+      .fetch(`${VITE_API_BASE_URL}/${RESORUCE_PATH}`)
       .then((response) => response.json())
-      .then((notes) => console.log(notes))
+      .then(({ notes }) => {
+        setNotes(notes);
+      })
       .catch((error) => console.log(error));
-  }, [API_BASE_URL]);
+  }, []);
 
   return (
     <>
-      <header>
+      <header className={styles["main-header"]}>
         <h1>Notekeeper</h1>
         <h2>
           A seamless app for organizing, managing, and accessing your notes.
         </h2>
         <small>{PACKAGE_VERSION}</small>
       </header>
+      <main>
+        <ul>
+          {notes.map((note) => {
+            return (
+              <li>
+                <h3>{note.name}</h3>
+                <p>{note.description}</p>
+                <p>{note.status}</p>
+                <p>{note.important.toString()}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
     </>
   );
 }
